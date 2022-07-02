b0f9954da910f1847d832f6833812211b5c85d2b
Racha777 <jramoschang@gmail.com>
2022-07-02 17:29:50 -0500

    Crud en api rest finalizado

--- semana03/dia5/apirest/run.py
+++ semana03/dia5/apirest/run.py
@@ -1,6 +1,16 @@
 from flask import Flask,jsonify,request
+from flask_mysqldb import MySQL
+import os
 
 app=Flask(__name__)
+
+#Valores de conexion a mysql
+app.config['MYSQL_HOST']=os.environ.get("MYSQL_ADDON_HOST")
+app.config['MYSQL_USER']=os.environ.get("MYSQL_ADDON_USER")
+app.config['MYSQL_PASSWORD']=os.environ.get("MYSQL_ADDON_PASSWORD")
+app.config['MYSQL_DB']=os.environ.get("MYSQL_ADDON_DB")
+
+mysql=MySQL(app)
 
 @app.route('/')
 def index():
@@ -9,6 +19,103 @@ def index():
         'content':'Bienvenido a mi api rest con flask y mysql'
     }
     return jsonify(context)
+
+#ENDPOINT ALUMNO
+@app.route('/alumno')
+def getAlumno():
+    cursor=mysql.connection.cursor()
+    cursor.execute('select * from tbl_alumno')
+
+    data=cursor.fetchall()
+    cursor.close()
+
+    context={
+        'status':True,
+        'content':data
+    }
+    return jsonify(context)
+
+@app.route('/alumno',methods=['POST'])
+def setAlumno():
+    nombre=request.json['nombre']
+    email=request.json['email']
+
+    cursor=mysql.connection.cursor()
+    cursor.execute("insert into tbl_alumno(alumno_nombre,alumno_email) values('"+nombre+"','"+email+"')")
+
+    mysql.connection.commit()
+    cursor.close()
+
+    context={
+        'status':True,
+        'content':'registro exitoso'
+    }
+    return jsonify(context)
+
+@app.route('/alumno/<id>')
+def getAlumnoById(id):
+    cursor=mysql.connection.cursor()
+    cursor.execute("select * from tbl_alumno where alumno_id='"+id+"'")
+
+    data=cursor.fetchall()
+
+    cursor.close()
+
+    context={
+        'status':True,
+        'content':data
+    }
+
+    return jsonify(context)
+
+@app.route('/alumno/<id>',methods=['PUT'])
+def updateAlumno(id):
+    nombre=request.json['nombre']
+    email=request.json['email']
+
+    cursor=mysql.connection.cursor()
+    sqlUpdateAlumno="""
+    update tbl_alumno set
+    alumno_nombre='"""+nombre+"""'
+    ,alumno_email='"""+email+"""'
+    where alumno_id='"""+id+"""'
+    """
+    cursor.execute(sqlUpdateAlumno)
+    mysql.connection.commit()
+    cursor.close()
+
+    context={
+        'status':True,
+        'content':'Registro actualizado'
+    }
+
+    return jsonify(context)
+
+@app.route('/alumno/<id>',methods=['DELETE'])
+def deleteAlumno(id):
+
+    try:
+        cursor=mysql.connection.cursor()
+        sqlDeleteAlumno="""
+        delete from tbl_alumno
+        where alumno_id='"""+id+"""'
+        """
+        cursor.execute(sqlDeleteAlumno)
+        mysql.connection.commit()
+        cursor.close()
+
+        context={
+            'status':True,
+            'content':'Registro eliminado'
+        }
+        return jsonify(context)
+    
+    except Exception as err:
+        context={
+            'status':False,
+            'content':'Error:' + err.__str__()
+        }
+        return jsonify(context),500
 
 if __name__=="__main__":
     app.run(debug=True,port=5000)
