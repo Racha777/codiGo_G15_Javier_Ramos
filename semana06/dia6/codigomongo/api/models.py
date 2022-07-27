f5634c3d745a2c932b6461a9eda62c313bd58c88
Racha777 <jramoschang@gmail.com>
2022-07-26 20:08:52 -0500

    Clase s6d6

---
+++ semana06/dia6/codigomongo/api/models.py
@@ -0,0 +1,17 @@
+#from django.db import models
+from mongoengine import Document,fields,EmbeddedDocument
+
+# Create your models here.
+class Alumno(Document):
+    nombre=fields.StringField()
+    email=fields.EmailField()
+    celular=fields.StringField()
+    github=fields.StringField()
+
+class Curso(EmbeddedDocument):
+    nombre=fields.StringField(required=True)
+
+class Matricula(Document):
+    nro=fields.StringField(required=True)
+    alumno=fields.ReferenceField('Alumno')
+    cursos=fields.ListField(fields.EmbeddedDocumentField(Curso))
---
+++ semana06/dia6/codigomongo/api/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from rest_framework import generics
+from rest_framework_mongoengine import generics as drfme_generics
+
+from .models import Alumno,Matricula
+from .serializers import AlumnoSerializer,MatriculaSerializer
+
+class AlumnoView(APIView):
+    def get(self,request):
+        data=Alumno.objects.all()
+        serData=AlumnoSerializer(data,many=True)
+        return Response(serData.data)
+
+    def post(self,request):
+        serData=AlumnoSerializer(data=request.data)
+        serData.is_valid(raise_exception=True)
+        serData.save()
+
+        return Response(serData.data)
+
+class MatriculaView(drfme_generics.ListCreateAPIView):
+    queryset=Matricula.objects.all()
+    serializer_class=MatriculaSerializer
