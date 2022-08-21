8ee02c7cb0d9d5c47e131a24afaab746e4630e85
Racha777 <jramoschang@gmail.com>
2022-08-21 17:55:33 -0500

    clase s10d6

---
+++ semana10/dia6/perudemy/frontend/src/modulos/cursos/pages/Cursos.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Catalogo from "../components/Catalogo";
+
+const Cursos=()=>{
+    return(
+        <div id="Wrapper">
+            <NavBar/>
+            <div class="page-spacer"></div>
+            <Catalogo/>
+        </div>
+    )
+}
+
+export default Cursos;
